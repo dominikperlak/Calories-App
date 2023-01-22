14f9b832874b84313b1e0496cd72886aed22d65b
dominikperlak <dominikperlak2@gmail.com>
2023-01-22 17:14:26 +0100

    Components stuff

--- src/App.js
+++ src/App.js
@@ -1,11 +1,47 @@
+import React ,{useState,useEffect} from 'react';
 import './App.css';
+import AppBar from './components/AppBar/AppBar';
+import AppControlsCounter from './components/AppControls/AppControlsCounter';
+import AppControlsDelete from './components/AppControls/AppControlsDelete';
+import AppControlsinputs from './components/AppControls/AppControlsinputs';
 const App = () => {
+    const[meals,setMeals] = useState([])
+    const[mealName,setMealName] = useState("");
+    const[calories,setCalories] = useState(0);
+    const[openModal,setOpenModal] = useState(false);
+
+  const addMealsHandler = () => {
+    const oldMeals = [...meals];
+    const meal ={
+      mealName,
+      calories,
+      id: Math.floor(Math.random()*1000),
+    };
+
+    const newMeals = oldMeals.concat(meal);
+
+    if(calories <=0 || mealName === ""){
+      alert("must not be empty")
+    }else{
+      setMeals(newMeals)
+    }
+
+    setMealName("")
+    setCalories(0);
+
+  }
+
   return (
  <div className='App'>
-  <h1>Hello From React</h1>
+  <AppBar />
+  <AppControlsCounter />
+  <AppControlsDelete />
+  < AppControlsinputs addMealsHandler={addMealsHandler} mealName={mealName} calories={calories} 
+  setMealName={setMealName} setCalories={setCalories}
+  />
  </div>
   );
-}
+};
   
 
 
